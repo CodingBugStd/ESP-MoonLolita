cec321fa2e2348e5dbf3e674712208fc3874c58f
庞碧璋 <11040312+pang-bizhang@user.noreply.gitee.com>
2022-10-03 23:16:55 +0800

    feat: ✨在win下随便写了UI界面

--- code/esp-idf-project/components/MoonLolita-UI/home_page/home_page.c
+++ code/esp-idf-project/components/MoonLolita-UI/home_page/home_page.c
@@ -1,22 +1,46 @@
 #include "home_page.h"
 
-static lv_obj_t * screen = NULL;
-static lv_obj_t * label = NULL;
+static void lable_create( lv_obj_t *parent ){
+    lv_label_t *label = NULL;
 
-static void home_page_obj_create(){
-    screen = lv_obj_create(NULL);
-    label = lv_label_create( screen );
-    lv_label_set_text( label , "HelloWorld!");
+    label = lv_label_create( parent );
+    lv_label_set_text(label,"HelloWorld!\nIMG Test");
 }
 
-static void home_page_obj_delete(){
-    lv_obj_del( screen );
-    screen = NULL;
-    label = NULL;
+static void button_callback( lv_event_t * event ){
+    //printf("HelloWorld!\n");
+}
+
+static void button_create( lv_obj_t * parent){
+    lv_btn_t * btn = lv_btn_create( parent );
+    lv_label_t * btn_label = lv_label_create( btn );
+
+    lv_obj_set_align( btn , LV_ALIGN_CENTER );
+    lv_obj_set_size( btn , 80 , 40 );
+
+    lv_label_set_text( btn_label , "test" );
+    lv_obj_set_align( btn_label , LV_ALIGN_CENTER );
+
+    lv_obj_set_style_bg_color( btn , lv_palette_main(LV_PALETTE_RED) , 0 );
+    lv_obj_set_style_bg_opa( btn , LV_OPA_60 , 0 );
+
+    lv_obj_add_event_cb( btn , button_callback , LV_EVENT_CLICKED , NULL );
+}
+
+static void home_page_create(){
+    lv_img_t *screen = NULL;
+
+    screen = lv_img_create( NULL );
+
+    //LV_IMG_DECLARE( img1 );
+    //lv_img_set_src( screen , &img1 );
+
+    lable_create( screen );
+    button_create( screen );
+    lv_scr_load( screen );
 }
 
 void home_page_load(){
-    home_page_obj_create();
-    lv_scr_load( screen );
+    home_page_create();
 }
 
